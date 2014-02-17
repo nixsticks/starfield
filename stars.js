f9077e0c474d8f35edce4b163aef804c3e81aa99
Nikki Thean <nikki.thean@gmail.com>
2014-02-17 10:02:25 -0500

    Update sprites

--- stars.js
+++ stars.js
@@ -41,7 +41,7 @@ $(document).ready(function()  {
   jQuery.get('stars.json', function(data) {
     var starData = data;
 
-    for (var i = 1; i < starData.length; i++) {
+    for (var i = 0; i < starData.length; i++) {
       var star = starData[i],
           pX = star.x,
           pY = star.y,
@@ -49,6 +49,12 @@ $(document).ready(function()  {
           newStar = new THREE.Vector3(pX, pY, pZ);
 
       stars.vertices.push(newStar);
+
+      if (star.propername) {
+        var sprite = makeTextSprite(star.propername, { fontsize: 32, backgroundColor: {r:255, g:100, b:100, a:1} } );
+        sprite.position = stars.vertices[i].clone().multiplyScalar(1.1);
+        scene.add( sprite );
+      }
     }
 
     var starSystem = new THREE.ParticleSystem(stars, starMaterial);
@@ -63,4 +69,62 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   controls.update();
+}
+
+function makeTextSprite( message, parameters ) {
+  if ( parameters === undefined ) parameters = {};
+  
+  var fontface = parameters.hasOwnProperty("fontface") ? 
+    parameters["fontface"] : "Arial";
+  
+  var fontsize = parameters.hasOwnProperty("fontsize") ? 
+    parameters["fontsize"] : 18;
+  
+  var borderThickness = parameters.hasOwnProperty("borderThickness") ? 
+    parameters["borderThickness"] : 4;
+  
+  var borderColor = parameters.hasOwnProperty("borderColor") ?
+    parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+  
+  var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+    parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+
+  //var spriteAlignment = parameters.hasOwnProperty("alignment") ?
+  //  parameters["alignment"] : THREE.SpriteAlignment.topLeft;
+
+  // var spriteAlignment = THREE.SpriteAlignment.topLeft;
+
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+  context.font = "Bold " + fontsize + "px " + fontface;
+    
+  // get size data (height depends only on font size)
+  var metrics = context.measureText( message );
+  var textWidth = metrics.width;
+  
+  // // background color
+  // context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+  //                 + backgroundColor.b + "," + backgroundColor.a + ")";
+  // // border color
+  // context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+  //                 + borderColor.b + "," + borderColor.a + ")";
+
+  // context.lineWidth = borderThickness;
+  // roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+  // // 1.4 is extra height factor for text below baseline: g,j,p,q.
+  
+  // text color
+  context.fillStyle = "red";
+
+  context.fillText( message, borderThickness, fontsize + borderThickness);
+  
+  // canvas contents will be used for a texture
+  var texture = new THREE.Texture(canvas) 
+  texture.needsUpdate = true;
+
+  var spriteMaterial = new THREE.SpriteMaterial( 
+    { map: texture, useScreenCoordinates: false } );
+  var sprite = new THREE.Sprite( spriteMaterial );
+  sprite.scale.set(100,50,1.0);
+  return sprite;  
 }
